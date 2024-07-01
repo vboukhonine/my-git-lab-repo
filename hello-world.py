f5f539db6cd0dd6d2997675ba1c35bfa5bb4faf0
Vera Boukhonine <veraboukhonine123@gmail.com>
2024-06-30 22:36:33 -0700

    Small updates

--- hello-world.py
+++ hello-world.py
@@ -7,6 +7,13 @@ Vera Boukhonine
 
 # Task 3
 from other_file import my_list
-print("My List: ",my_list)
+
+print("My List: ", my_list)
 # Task 2
 print("Hello World!")
+
+"""
+Lab Completion Report: 
+I was able to complete the lab without too difficulty. I just ran my python code in VS code since I installed various python
+extensions to help with it. I also had some fun playing the git branching game.
+"""
